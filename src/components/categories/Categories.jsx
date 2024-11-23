e03c4b71627408724ce743ebf90aaba8617b17a8
ozgurdayanir <ozgurdayanir@gmail.com>
2024-11-23 16:19:04 +0300

    modal paths updated

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Modal from "../../modal/Modal";
+import Modal from "../../modal"
 import CategoryForm from "./CategoryForm";
 import { toast } from 'react-toastify';
 
--- src/components/publisher/Publisher.jsx
+++ src/components/publisher/Publisher.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Modal from "../../modal/idex.js";
+import Modal from "../../modal/index.js";
 import PublisherForm from "./PublisherForm";
 import { toast } from 'react-toastify';
 
